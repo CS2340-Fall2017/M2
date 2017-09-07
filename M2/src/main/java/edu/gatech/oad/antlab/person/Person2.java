7f826d2a36536294f5f612e752a6e86d02908640
Murphy <Murphy@lawn-128-61-48-141.lawn.gatech.edu>
2017-09-07 10:26:12 -0400

    Murphy's files.

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,9 +1,11 @@
 package edu.gatech.oad.antlab.person;
+import java.util.Collections;
+import java.util.ArrayList;
 
 /**
  *  A simple class for person 2
  *  returns their name and a
- *  modified string 
+ *  modified string
  *
  * @author Bob
  * @version 1.1
@@ -31,14 +33,27 @@ public class Person2 {
 	 */
 	private String calc(String input) {
 	  //Person 2 put your implementation here
-	  return null;
+    ArrayList<String> list = new ArrayList<>();
+
+    for (int i = 0; i < input.length(); i++) {
+         list.add(input.substring(i, i + 1));
+       }
+
+    Collections.shuffle(list);
+    String newString = "";
+
+    for (String l : list) {
+      newString = newString + l;
+    }
+
+	  return newString;
 	}
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
